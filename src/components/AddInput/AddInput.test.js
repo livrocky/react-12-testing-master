3ff3fd9746f94f0227b131a6e41b45cc5f60ac92
rokas <rokasra@gmail.com>
2022-06-27 11:46:15 +0300

    mock fn mock arr

--- src/components/AddInput/AddInput.test.js
+++ src/components/AddInput/AddInput.test.js
@@ -1,11 +1,41 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import AddInput from './AddInput';
 
-it('Add new task here', () => {
+it("it shout render input with placeholder 'Add a new task here...'", () => {
   // Arrange
-  render(<AddInput />);
-  // Act
+  render(<AddInput todos={[]} />);
   const inputEl = screen.getByPlaceholderText('Add a new task here...');
-  // Assert
   expect(inputEl).toBeInTheDocument();
+});
+
+it('should be able to type in input', () => {
+  // Arrange
+  render(<AddInput todos={[]} />);
+  const inputEl = screen.getByPlaceholderText('Add a new task here...');
+  // Act
+  // fireEvent.veiksmas(elementas, eventObjektas)
+  fireEvent.change(inputEl, { target: { value: 'bubble gum' } });
+  // assert
+  expect(inputEl).toHaveValue('bubble gum');
+  expect(inputEl.value).toBe('bubble gum');
+});
+
+// tuscia jest funkcija
+const mockFn = jest.fn();
+
+it('should clear input on add btb click', () => {
+  // Arrange
+  render(<AddInput setTodos={mockFn} todos={[]} />);
+  // paimti input
+  const inputEl = screen.getByPlaceholderText('Add a new task here...');
+
+  // irasyti teksta
+  fireEvent.change(inputEl, { target: { value: 'bubble gum' } });
+
+  // pasiimti mygtuka
+  const btnEl = screen.getByRole('button', { name: /add/i });
+  // paspausti mygtuka
+  fireEvent.click(btnEl);
+  // expectinti kad input yra tucias
+  expect(inputEl.value).toBe('');
 });
