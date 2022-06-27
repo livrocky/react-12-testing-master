f639c4b1375142e764f577d3f6e6b08b6bf6dd73
rokas <rokasra@gmail.com>
2022-06-27 09:36:27 +0300

    2 headings test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -6,8 +6,29 @@ it('renders Header', () => {
 });
 
 it('renders title from props', () => {
+  // Arrange
   render(<Header title='testing123' />);
   // const headerEl = screen.getByText(/testing123/i);
+  // Act
   const headerEl = screen.getByText('testing123');
+  // Assert
   expect(headerEl).toBeInTheDocument();
+});
+
+// it('should render a heading', () => {
+//   // Arrange
+//   render(<Header title='todos' />);
+//   // Search
+//   const headingEl = screen.getByRole('heading');
+//   // Assert
+//   expect(headingEl).toBeInTheDocument();
+// });
+
+it('should render 2 headings', () => {
+  // Arrange
+  render(<Header title='todos' />);
+  // Search
+  const headingElArr = screen.getAllByRole('heading');
+  // Assert
+  expect(headingElArr).toHaveLength(2);
 });
