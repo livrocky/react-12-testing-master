32777b60cc2cdece0b53cc45b8c691bf34e517a9
rokas <rokasra@gmail.com>
2022-06-28 08:58:17 +0300

    key prop

--- src/components/Todo/Todo.test.js
+++ src/components/Todo/Todo.test.js
@@ -8,18 +8,38 @@ const TodoWithRouter = (args) => (
   </BrowserRouter>
 );
 
+// const addTasks = (tasksArray) => {
+//   const inputEl = screen.getByPlaceholderText('Add a new task here...');
+//   const btnEl = screen.getByRole('button', { name: /add/i });
+//   tasksArray.forEach((task) => {
+//     fireEvent.change(inputEl, { target: { value: task } });
+//     fireEvent.click(btnEl);
+//   });
+// };
+
 // integration test
 it('should add new todo to todo list', () => {
-  //
+  // it should work with multiple todos
+
   render(<TodoWithRouter />);
+  // addTasks('Buy milk', 'go running', 'Do homework');
   const inputEl = screen.getByPlaceholderText('Add a new task here...');
-  fireEvent.change(inputEl, { target: { value: 'bubble gum' } });
   const btnEl = screen.getByRole('button', { name: /add/i });
+  fireEvent.change(inputEl, { target: { value: 'bubble gum' } });
   fireEvent.click(btnEl);
-  const divEl = screen.getByText('bubble gum');
-  expect(divEl).toBeInTheDocument();
+  fireEvent.change(inputEl, { target: { value: 'bubble gum' } });
+  fireEvent.click(btnEl);
+  fireEvent.change(inputEl, { target: { value: 'bubble gum' } });
+  fireEvent.click(btnEl);
+  const divEls = screen.getAllByTestId('single-todo');
+  expect(divEls).toHaveLength(3);
 });
 
-// it should work with multiple todos
-
 // it should render diffenere class when clicked on todo
+// it('it should render "todo-item-active" class when clicked on todo', () => {
+//   render(<TodoWithRouter />)
+//   addTasks(['bubble gum'])
+//   const divEl = screen.getByTestId('single-todo'
+//   fireEvent.click(divEl));
+//   expect(divEl).toHaveClass('todo-item-active')
+// })
